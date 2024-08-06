f31c5a1164b17c1a3c2320a3241632372fa8629c
RECodeVault <rlnvns@gmail.com>
2024-08-05 20:37:09 -0700

    Base piano tile bot

--- main.py
+++ main.py
@@ -1,16 +1,61 @@
-# This is a sample Python script.
-
-# Press Shift+F10 to execute it or replace it with your code.
-# Press Double Shift to search everywhere for classes, files, tool windows, actions, and settings.
-
-
-def print_hi(name):
-    # Use a breakpoint in the code line below to debug your script.
-    print(f'Hi, {name}')  # Press Ctrl+F8 to toggle the breakpoint.
+import numpy as np
+import pyautogui
+import cv2
+import keyboard
+import time
+import threading
 
 
-# Press the green button in the gutter to run the script.
-if __name__ == '__main__':
-    print_hi('PyCharm')
+def take_screenshot():
+    """
+    Takes screenshot and returns image as a numpy array
+    :return: Image
+    """
+    image = pyautogui.screenshot(region=(665, 165, 551, 850))
+    return cv2.cvtColor(np.array(image), cv2.COLOR_BGR2GRAY)
 
-# See PyCharm help at https://www.jetbrains.com/help/pycharm/
+
+def is_tile(pixel_value):
+    return pixel_value == 0
+
+
+def move_and_click(x, y):
+    pyautogui.moveTo(x, y)
+    pyautogui.click()
+
+
+def main():
+    print("Ready: ")
+    count = 5
+    while count > 0:
+        print(count)
+        count -= 1
+        time.sleep(1)
+
+    bar_width = 1
+    positions = [44, 187, 326, 472]
+    step = 50
+
+    while True:
+        if keyboard.is_pressed('s'):
+            exit()
+
+        image_array = take_screenshot()
+        found_tile = False
+
+        for y in reversed(range(0, len(image_array), step)):
+            if found_tile:
+                break
+            for x_start in positions:
+                for x in range(x_start, x_start + bar_width, step):
+                    pixel = image_array[y, x]
+                    if is_tile(pixel):
+                        threading.Thread(target=move_and_click, args=(x + 665, y + 180)).start()
+                        found_tile = True
+                        break
+                if found_tile:
+                    break
+
+
+if __name__ == "__main__":
+    main()
